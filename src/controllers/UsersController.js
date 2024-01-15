6057b92378902b65fd98e749ff4a12950f88a38c
Samuel <samuelfreitasmendes@yahoo.com.br>
2024-01-15 19:38:05 +0000

    database created

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,14 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
-  create(request, response) {
+   create(request, response) {
     const {name, email, password} = request.body
 
-    response.json({ name, email, password})
+    if(!name) {
+      throw new AppError("O NOME É OBRIGATORIO");
+    }
+
+    response.status(201).json({ name, email, password})
   }
 }
 
