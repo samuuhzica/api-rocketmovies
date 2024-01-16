9e312286d6e9c0d7fcdeea4ea7d125c3090804aa
Samuel <samuelfreitasmendes@yahoo.com.br>
2024-01-16 15:37:39 +0000

    user update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,62 @@
-const AppError = require("../utils/AppError")
+const { hash } = require('bcryptjs')
+
+const AppError = require('../utils/AppError')
+
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
-   create(request, response) {
-    const {name, email, password} = request.body
+  async create(request, response) {
+    const { name, email, password } = request.body
 
-    if(!name) {
-      throw new AppError("O NOME É OBRIGATORIO");
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (checkUserExists) {
+      throw new AppError('Este email ja esta em uso')
     }
 
-    response.status(201).json({ name, email, password})
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?,?,?)',
+      [name, email, hashedPassword]
+    )
+
+    return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user) {
+      throw new AppError("Usuario nao encontrado")
+    }
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== id){
+      throw new AppError("Este email ja esta em uso.")
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+      )
+
+      return response.json()
   }
 }
 
